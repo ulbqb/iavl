da3496440c02bec93c894113b1e337634c3bd3af
Manav Aggarwal <manavaggarwal1234@gmail.com>
2022-11-10 12:21:09 -0800

    feat: Support Adds in a Deep Subtree (#8)
    * ICS23-style approach for DeepSubTree creation
    
    * Fix deepsubtree, all checks pass
    
    * Update documentation for deep subtrees
    
    * Fix deepsubtree to accomodate left/right paths both
    
    * Refactor test code
    
    * Refactor TestDeepSubtreeStepByStep
    
    * Address comments
    
    * Refactor code
    
    * Disable gocritic and unused
    
    * Add description
    
    * Refactor code to check err in Set
    
    * Modify traversal conditions to be clearer
    
    * feat: build deep subtree from ICS23 inclusion proofs (#9)
    
    * feat: build deep subtree from ICS23 inclusion proofs
    
    * feat: handle non-existence proofs
    
    * linter: goimports deepsubtree.go
    
    * refactor: addExistenceProofProof -> addExistenceProof
    
    * fix: un-hardcode size of byte array
    
    * Add more comments
    
    * Refactor ndb.Commit call outside for loop
    
    * verify that in the case that dst.root != nil that the root node hash matches the provided hash and check dst.root != nil first
    
    * Add strings and use strings.Repeat
    
    * Delete addPath and AddPath
    
    * Remove print statements
    
    * Refactor recomputeHash usage
    
    Co-authored-by: Matthew Sevey <mjsevey@gmail.com>
    
    * return err directly
    
    Co-authored-by: Matthew Sevey <mjsevey@gmail.com>
    
    * Address linting checks
    
    * Use tm-db instead of cosmos-db
    
    * Fix linter
    
    * Add test to check add operation in deepsubtree: WIP
    
    * Add insert key functionality WIP
    
    * Try replicating non-inclusion proofs
    
    * Add sibling nodes to nonExistenceProofs
    
    * Refactor test code
    
    * Finish adding add functionality to dst
    
    * Add Remove functionality to dst
    
    * Fix linting errors
    
    * Fix GetSiblingNode
    
    * Remove more print statements
    
    * Add comment for each case in recursive set
    
    * Change which methods are exported and document exported methods
    
    * feat: Support Empty Hashes and Add constructor (#11)
    
    * Export siblings
    
    * Add deepsubtree constructor
    
    * Support empty root hashes
    
    * Use working hash instead of root.hash
    
    * Use tm-db instead of cosmos-db
    
    * Return nil in BuildTree
    
    * Address comments
    
    * Address more comments
    
    Co-authored-by: Tomasz Zdybał <tomek@zdybal.lap.pl>
    Co-authored-by: Matthew Sevey <mjsevey@gmail.com>

--- deepsubtree_test.go
+++ deepsubtree_test.go
@@ -1,11 +1,50 @@
 package iavl
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	db "github.com/tendermint/tm-db"
 )
+
+// Returns whether given trees have equal hashes
+func haveEqualRoots(tree1 *MutableTree, tree2 *MutableTree) (bool, error) {
+	rootHash, err := tree1.WorkingHash()
+	if err != nil {
+		return false, err
+	}
+
+	treeWorkingHash, err := tree2.WorkingHash()
+	if err != nil {
+		return false, err
+	}
+
+	// Check root hashes are equal
+	return bytes.Equal(rootHash, treeWorkingHash), nil
+}
+
+// Tests creating an empty Deep Subtree
+func TestEmptyDeepSubtree(t *testing.T) {
+	require := require.New(t)
+	getTree := func() *MutableTree {
+		tree, err := getTestTree(0)
+		require.NoError(err)
+		return tree
+	}
+
+	tree := getTree()
+	rootHash, err := tree.WorkingHash()
+	require.NoError(err)
+
+	dst := NewDeepSubTree(db.NewMemDB(), 100, false, 0)
+	err = dst.BuildTree(rootHash)
+	require.NoError(err)
+
+	areEqual, err := haveEqualRoots(dst.MutableTree, tree)
+	require.NoError(err)
+	require.True(areEqual)
+}
 
 // Tests creating a Deep Subtree step by step
 // as a full IAVL tree and checks if roots are equal
@@ -27,11 +66,10 @@ func TestDeepSubtreeStepByStep(t *testing.T) {
 	}
 
 	tree := getTree()
-	rootHash := tree.root.hash
-
-	mutableTree, err := NewMutableTree(db.NewMemDB(), 100, false)
+	rootHash, err := tree.WorkingHash()
 	require.NoError(err)
-	dst := DeepSubTree{mutableTree}
+
+	dst := NewDeepSubTree(db.NewMemDB(), 100, false, 0)
 
 	// insert key/value pairs in tree
 	allkeys := [][]byte{
@@ -42,15 +80,16 @@ func TestDeepSubtreeStepByStep(t *testing.T) {
 	for _, key := range allkeys {
 		ics23proof, err := tree.GetMembershipProof(key)
 		require.NoError(err)
-		err = dst.AddProof(ics23proof)
+		err = dst.AddExistenceProof(ics23proof.GetExist())
 		require.NoError(err)
 
 		err = dst.BuildTree(rootHash)
 		require.NoError(err)
 	}
 
-	// Check root hashes are equal
-	require.Equal(dst.root.hash, tree.root.hash)
+	areEqual, err := haveEqualRoots(dst.MutableTree, tree)
+	require.NoError(err)
+	require.True(areEqual)
 }
 
 // Tests updating the deepsubtree returns the
@@ -84,22 +123,24 @@ func TestDeepSubtreeWithUpdates(t *testing.T) {
 
 	for _, subsetKeys := range testCases {
 		tree := getTree()
-		rootHash := tree.root.hash
+		rootHash, err := tree.WorkingHash()
+		require.NoError(err)
 		mutableTree, err := NewMutableTree(db.NewMemDB(), 100, false)
 		require.NoError(err)
 		dst := DeepSubTree{mutableTree}
 		for _, subsetKey := range subsetKeys {
 			ics23proof, err := tree.GetMembershipProof(subsetKey)
 			require.NoError(err)
-			err = dst.AddProof(ics23proof)
-			require.NoError(err)
-			dst.BuildTree(rootHash)
+			err = dst.AddExistenceProof(ics23proof.GetExist())
 			require.NoError(err)
 		}
+		dst.BuildTree(rootHash)
+		require.NoError(err)
 		dst.SaveVersion()
 
-		// Check root hashes are equal
-		require.Equal(dst.root.hash, tree.root.hash)
+		areEqual, err := haveEqualRoots(dst.MutableTree, tree)
+		require.NoError(err)
+		require.True(areEqual)
 
 		values := [][]byte{{10}, {20}}
 		for i, subsetKey := range subsetKeys {
@@ -109,7 +150,100 @@ func TestDeepSubtreeWithUpdates(t *testing.T) {
 			tree.SaveVersion()
 		}
 
-		// Check root hashes are equal
-		require.Equal(dst.root.hash, tree.root.hash)
+		areEqual, err = haveEqualRoots(dst.MutableTree, tree)
+		require.NoError(err)
+		require.True(areEqual)
+	}
+}
+
+// Tests adding and deleting keys in the deepsubtree returns the
+// correct roots
+func TestDeepSubtreeWWithAddsAndDeletes(t *testing.T) {
+	require := require.New(t)
+	getTree := func() *MutableTree {
+		tree, err := getTestTree(5)
+		require.NoError(err)
+
+		tree.Set([]byte("b"), []byte{2})
+		tree.Set([]byte("a"), []byte{1})
+
+		_, _, err = tree.SaveVersion()
+		require.NoError(err)
+		return tree
+	}
+	tree := getTree()
+
+	subsetKeys := [][]byte{
+		[]byte("b"),
+	}
+	rootHash, err := tree.WorkingHash()
+	require.NoError(err)
+	mutableTree, err := NewMutableTree(db.NewMemDB(), 100, false)
+	require.NoError(err)
+	dst := DeepSubTree{mutableTree}
+	for _, subsetKey := range subsetKeys {
+		ics23proof, err := tree.GetMembershipProof(subsetKey)
+		require.NoError(err)
+		err = dst.AddExistenceProof(ics23proof.GetExist())
+		require.NoError(err)
+	}
+
+	keysToAdd := [][]byte{
+		[]byte("c"), []byte("d"),
+	}
+	valuesToAdd := [][]byte{
+		{3}, {4},
+	}
+	// Add non-existence proofs for keys we expect to add later
+	for _, keyToAdd := range keysToAdd {
+		ics23proof, err := tree.GetNonMembershipProof(keyToAdd)
+		require.NoError(err)
+		dst_nonExistenceProof, err := ConvertToDSTNonExistenceProof(tree, ics23proof.GetNonexist())
+		require.NoError(err)
+		dst.AddNonExistenceProof(dst_nonExistenceProof)
+		require.NoError(err)
+		dst.BuildTree(rootHash)
+		require.NoError(err)
+	}
+	dst.SaveVersion()
+
+	areEqual, err := haveEqualRoots(dst.MutableTree, tree)
+	require.NoError(err)
+	require.True(areEqual)
+
+	require.Equal(len(keysToAdd), len(valuesToAdd))
+	// Add all the keys we intend to add and check root hashes stay equal
+	for i := range keysToAdd {
+		keyToAdd := keysToAdd[i]
+		valueToAdd := valuesToAdd[i]
+		dst.Set(keyToAdd, valueToAdd)
+		dst.SaveVersion()
+		rootHash, err := dst.WorkingHash()
+		require.NoError(err)
+		err = dst.BuildTree(rootHash)
+		require.NoError(err)
+		tree.Set(keyToAdd, valueToAdd)
+		tree.SaveVersion()
+
+		areEqual, err := haveEqualRoots(dst.MutableTree, tree)
+		require.NoError(err)
+		require.True(areEqual)
+	}
+
+	// Delete all the keys we added and check root hashes stay equal
+	for i := range keysToAdd {
+		keyToAdd := keysToAdd[i]
+		dst.Remove(keyToAdd)
+		dst.SaveVersion()
+		rootHash, err := dst.WorkingHash()
+		require.NoError(err)
+		err = dst.BuildTree(rootHash)
+		require.NoError(err)
+		tree.Remove(keyToAdd)
+		tree.SaveVersion()
+
+		areEqual, err := haveEqualRoots(dst.MutableTree, tree)
+		require.NoError(err)
+		require.True(areEqual)
 	}
 }
