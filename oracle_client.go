69b2cffe01001a1e597f7199861c7e6f47e24ad2
Shogo Hyodo <mmoshg8u@gmail.com>
2023-06-14 17:30:22 +0900

    Update

--- oracle_client.go
+++ oracle_client.go
@@ -4,14 +4,20 @@ import (
 	"bytes"
 	"encoding/hex"
 	"fmt"
+	"net/url"
 
 	ics23 "github.com/confio/ics23/go"
+	abci "github.com/tendermint/tendermint/abci/types"
+	"github.com/tendermint/tendermint/crypto/merkle"
+	"github.com/tendermint/tendermint/proto/tendermint/crypto"
 )
 
+// OracleClient Interface
 type OracleClientI interface {
-	GetProof(string, string) []*ics23.CommitmentProof
+	Get([]byte) []byte
 }
 
+// OracleClient
 type OracleClient struct {
 	oracle      OracleClientI
 	accessedKey AccessedKey
@@ -26,22 +32,44 @@ func NewOracleClient(oracle OracleClientI, storeName string) *OracleClient {
 	}
 }
 
-func (c *OracleClient) GetProof(path, data string) ([]*ics23.CommitmentProof, bool) {
+func (c *OracleClient) query(path, data string) *abci.ResponseQuery {
+	d := c.oracle.Get([]byte(fmt.Sprintf("abci_query?path=%s&data=%s", url.PathEscape(path), data)))
+	q := abci.ResponseQuery{}
+	err := q.Unmarshal(d)
+	if err != nil {
+		panic(err)
+	}
+	return &q
+}
+
+func (c *OracleClient) getProof(path, data string) ([]*ics23.CommitmentProof, bool) {
 	if c.accessedKey.Has(path, data) {
 		return nil, true
 	}
 	c.accessedKey.Set(path, data)
-	return c.oracle.GetProof(path, data), false
+
+	q := c.query("store/"+path, data)
+	ops := make([]*crypto.ProofOp, len(q.ProofOps.Ops))
+	for i := range q.ProofOps.Ops {
+		op := q.ProofOps.Ops[i]
+		ops[i] = &op
+	}
+
+	cops, err := convertToCommitmentProofs(ops)
+	if err != nil {
+		panic(err)
+	}
+	return cops, false
 }
 
 func (c *OracleClient) GetRootHash() []byte {
-	proofs, _ := c.GetProof(c.keyPath(), dataString([]byte("roothash")))
+	proofs, _ := c.getProof(c.keyPath(), dataString([]byte("roothash")))
 	rootHash := proofs[len(proofs)-1].GetExist().Value
 	return rootHash
 }
 
 func (c *OracleClient) GetNodesWithKey(key []byte) ([]*Node, bool) {
-	ps, accessed := c.GetProof(c.keysPath(), dataString(key))
+	ps, accessed := c.getProof(c.keysPath(), dataString(key))
 	if accessed {
 		return nil, accessed
 	}
@@ -113,7 +141,7 @@ func (c *OracleClient) GetNodesWithKey(key []byte) ([]*Node, bool) {
 }
 
 func (c *OracleClient) GetNode(hash []byte) (*Node, bool) {
-	proofs, accessed := c.GetProof(c.nodePath(), dataString(hash))
+	proofs, accessed := c.getProof(c.nodePath(), dataString(hash))
 	if accessed {
 		return nil, accessed
 	}
@@ -157,23 +185,6 @@ func (c *OracleClient) GetNode(hash []byte) (*Node, bool) {
 	return node, false
 }
 
-func getExistenceProof(cp *ics23.CommitmentProof) []*ics23.ExistenceProof {
-	eps := []*ics23.ExistenceProof{}
-	switch cp.GetProof().(type) {
-	case *ics23.CommitmentProof_Exist:
-		eps = append(eps, cp.GetExist())
-	case *ics23.CommitmentProof_Nonexist:
-		nep := cp.GetNonexist()
-		if nep.Left != nil {
-			eps = append(eps, nep.Left)
-		}
-		if nep.Right != nil {
-			eps = append(eps, nep.Right)
-		}
-	}
-	return eps
-}
-
 func (c *OracleClient) keyPath() string {
 	return fmt.Sprintf("%s/key", c.storeName)
 }
@@ -186,10 +197,60 @@ func (c *OracleClient) nodePath() string {
 	return fmt.Sprintf("%s/node", c.storeName)
 }
 
-func dataString(b []byte) string {
-	return hex.EncodeToString(b)
+// CommitmentOp
+type CommitmentOp struct {
+	Type  string
+	Spec  *ics23.ProofSpec
+	Key   []byte
+	Proof *ics23.CommitmentProof
 }
 
+var _ merkle.ProofOperator = CommitmentOp{}
+
+func (op CommitmentOp) GetKey() []byte {
+	return op.Key
+}
+
+func (op CommitmentOp) ProofOp() crypto.ProofOp {
+	bz, err := op.Proof.Marshal()
+	if err != nil {
+		panic(err.Error())
+	}
+	return crypto.ProofOp{
+		Type: op.Type,
+		Key:  op.Key,
+		Data: bz,
+	}
+}
+
+func (op CommitmentOp) Run(args [][]byte) ([][]byte, error) {
+	// calculate root from proof
+	root, err := op.Proof.Calculate()
+	if err != nil {
+		return nil, fmt.Errorf("could not calculate root for proof: %v", err)
+	}
+	// Only support an existence proof or nonexistence proof (batch proofs currently unsupported)
+	switch len(args) {
+	case 0:
+		// Args are nil, so we verify the absence of the key.
+		absent := ics23.VerifyNonMembership(op.Spec, root, op.Proof, op.Key)
+		if !absent {
+			return nil, fmt.Errorf("proof did not verify absence of key: %s", string(op.Key))
+		}
+
+	case 1:
+		// Args is length 1, verify existence of key with value args[0]
+		if !ics23.VerifyMembership(op.Spec, root, op.Proof, op.Key, args[0]) {
+			return nil, fmt.Errorf("proof did not verify existence of key %s with given value %x", op.Key, args[0])
+		}
+	default:
+		return nil, fmt.Errorf("args must be length 0 or 1, got: %d", len(args))
+	}
+
+	return [][]byte{root}, nil
+}
+
+// NodeSet
 type NodeSet map[string]*Node
 
 func (s NodeSet) Set(n *Node) {
@@ -215,6 +276,7 @@ func (s NodeSet) List() []*Node {
 	return ns
 }
 
+// AccessedKey
 type AccessedKey map[string]map[string]struct{}
 
 func (ak AccessedKey) Set(k1, k2 string) {
@@ -232,4 +294,69 @@ func (ak AccessedKey) Has(k1, k2 string) bool {
 	}
 	_, ok = ak[k1][k2]
 	return ok
+}
+
+// helper
+func getExistenceProof(cp *ics23.CommitmentProof) []*ics23.ExistenceProof {
+	eps := []*ics23.ExistenceProof{}
+	switch cp.GetProof().(type) {
+	case *ics23.CommitmentProof_Exist:
+		eps = append(eps, cp.GetExist())
+	case *ics23.CommitmentProof_Nonexist:
+		nep := cp.GetNonexist()
+		if nep.Left != nil {
+			eps = append(eps, nep.Left)
+		}
+		if nep.Right != nil {
+			eps = append(eps, nep.Right)
+		}
+	}
+	return eps
+}
+
+func convertToCommitmentProofs(ops []*crypto.ProofOp) ([]*ics23.CommitmentProof, error) {
+	cps := make([]*ics23.CommitmentProof, 0)
+	for _, op := range ops {
+		op, err := commitmentOpDecoder(*op)
+		if err != nil {
+			return nil, err
+		}
+		commitmentOp := op.(CommitmentOp)
+		commitmentProof := commitmentOp.Proof
+		if err != nil {
+			return nil, err
+		}
+		cps = append(cps, commitmentProof)
+	}
+	return cps, nil
+}
+
+func commitmentOpDecoder(pop crypto.ProofOp) (merkle.ProofOperator, error) {
+	var spec *ics23.ProofSpec
+	switch pop.Type {
+	case "ics23:iavl":
+		spec = ics23.IavlSpec
+	case "ics23:simple":
+		spec = ics23.TendermintSpec
+	default:
+		return nil, fmt.Errorf("unexpected ProofOp.Type; got %s, want supported ics23 subtypes 'ProofOpIAVLCommitment' or 'ProofOpSimpleMerkleCommitment'", pop.Type)
+	}
+
+	proof := &ics23.CommitmentProof{}
+	err := proof.Unmarshal(pop.Data)
+	if err != nil {
+		return nil, err
+	}
+
+	op := CommitmentOp{
+		Type:  pop.Type,
+		Key:   pop.Key,
+		Spec:  spec,
+		Proof: proof,
+	}
+	return op, nil
+}
+
+func dataString(b []byte) string {
+	return hex.EncodeToString(b)
 }
